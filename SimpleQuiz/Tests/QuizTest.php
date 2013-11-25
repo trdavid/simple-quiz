596aca8dc77431f620adc854483d475fccc40ee0
ElanMan <ben@elanman.com>
2013-11-25 20:20:12 +0000

    Trying out travis-ci build

--- SimpleQuiz/Tests/QuizTest.php
+++ SimpleQuiz/Tests/QuizTest.php
@@ -11,23 +11,16 @@ class QuizTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        parent::setUp();
-        \ORM::configure('mysql:host=' . \SimpleQuiz\Utils\Base\Config::$dbhost. ';dbname=' . \SimpleQuiz\Utils\Base\Config::$dbname);
-        \ORM::configure('username', \SimpleQuiz\Utils\Base\Config::$dbuser);
-        \ORM::configure('password', \SimpleQuiz\Utils\Base\Config::$dbpassword);
+        \ORM::configure('mysql:dbname=simple-quiz;host=localhost');
+        \ORM::configure('username', 'travis');
+        \ORM::configure('password', '');
         \ORM::configure('return_result_sets', true);
-        
-        $this->app = new \Slim\Helper\Set(array(
-            'debug' => true,
-            'log.enabled' => true,
-            'templates.path' => '../templates'
-        ));
+
+        $this->app = new \Slim\Helper\Set();
         $this->app->leaderboard = function() {
             return new \SimpleQuiz\Utils\LeaderBoard();
         };
-        
         $this->quiz = new Quiz($this->app);
-        
     }
 
     public function testsetId()
